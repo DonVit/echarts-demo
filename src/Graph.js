eda1ae7882d810b28b7fd71e37d2e7764632c916
Vitalie Doni <Vitalie.Doni@endava.com>
2022-11-08 17:16:59 +0200

    add echarts example

---
+++ src/Graph.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactECharts from 'echarts-for-react'
+import GrapthData from './GraphData'
+
+const Graph = () =>
+
+<ReactECharts
+  option={GrapthData}
+/>
+
+export default Graph
